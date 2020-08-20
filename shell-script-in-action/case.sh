397339285590d7fd3232c006e953b8e66e22ab77
biningo <biningo@yeah.net>
2020-08-20 19:51:55 +0800

    case

--- shell-script-in-action/case.sh
+++ shell-script-in-action/case.sh
@@ -5,3 +5,14 @@ case $1 in
   c) echo "C" ;;
   *) echo "Other" ;;
 esac
+
+read -p "输入一个字母或则数字:" input
+
+case "$input" in 
+  [[:lower:]] | [[:upper:]] ) echo "输入了字母: $input" ;;
+  [[:digit:]]) echo "输入了数字:$input" ;; #只能匹配一位数字
+  *) echo "输入了其他"  ;;
+esac
+
+
+
